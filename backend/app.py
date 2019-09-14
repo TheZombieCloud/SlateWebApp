379a2021e8f5f0307050ac666b3fd4d906729f11
Joshua Cheng <josh510cheng@gmail.com>
2019-09-14 18:44:34 -0400

    Database Made
    Created the database for the users with SQLite

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=False)
+    email = db.Column(db.String(120), nullable=False, unique=False)
+    password = db.Column(db.String(60), nullable=False, unique=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email},' '{self.password}')"
 
 
 @app.route('/')

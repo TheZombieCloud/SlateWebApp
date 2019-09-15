71fff9e3dcb8bebe526d7e9a52aa15719ee78c74
Alexander Ma <alex.magamer@gmail.com>
2019-09-14 22:33:45 -0400

    Schedule Complete

--- slate/src/Schedule.js
+++ slate/src/Schedule.js
@@ -2,19 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class ScheduleBlock {
-    constructor(name, duration, start, end) {
-        this.state = {name: name, duration: duration, start: start, end: end};
+    constructor(name, duration, start, end, day) {
+        this.state = {name: name, duration: duration, start: start, end: end, day: day};
     }
 }
 
 class Schedule extends React.Component {
+    static blocks = [];
+
     constructor(){
         super();
-        this.state = {timetable: [144][7]};
+        this.state = {timetable: []};
+        for (var i = 0;i<144;i++){
+            this.state.timetable[i] = [];
+            for (var c = 0;c<7;c++){
+                this.state.timetable[i][c] = "0";
+            }
+        }
+        Schedule.addScheduleBlock(new ScheduleBlock("SE101", 100, "1:00","2:40", 1));
+        this.state.timetable = Schedule.addTimeTable(this.state.timetable);
     }
 
-    addTimeTable(){
+    static addScheduleBlock(block){
+        //Retrieves schedule blocks from Database and then adds new block
+        this.blocks.push(block);
+    }
 
+    static addTimeTable(timetable){
+        for (var i = 0;i<this.blocks.length;i++){
+            var name = this.blocks[i].state.name;
+            var duration = this.blocks[i].state.duration;
+            var start = this.blocks[i].state.start;
+            var end = this.blocks[i].state.end;
+            var day = this.blocks[i].state.day;
+            var temp = start.split(":");
+            var time = Math.floor((parseInt(temp[0])*60+parseInt(temp[1]))/10);
+            timetable[time][day] = name + " - [" + start + "-" + end + "]";
+            for (var i = 1;i<=duration/10;i++){
+                timetable[time+i][day] = 1;
+            }
+        }
+        return timetable;
     }
 
     render(){
@@ -41,6 +69,20 @@ class Schedule extends React.Component {
                 schedule[i][0] = (<td>
                                     <h3></h3>
                                   </td>);
+            }
+        }
+        for (var i = 1;i<145;i++){
+            for (var c = 1;c<8;c++){
+                if (this.state.timetable[i-1][c-1]!=0) {
+                    schedule[i][c] = (<td bgcolor="green">
+                        <h3>{this.state.timetable[i - 1][c - 1]}</h3>
+                    </td>);
+                }
+                else {
+                    schedule[i][c] = (<td>
+                        <h3>{this.state.timetable[i - 1][c - 1]}</h3>
+                    </td>);
+                }
             }
         }
         var format = [145];

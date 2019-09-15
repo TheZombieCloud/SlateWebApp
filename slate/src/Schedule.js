4bb0333d0eaf474e8124046e47382537c67ebbb0
Alexander Ma <alex.magamer@gmail.com>
2019-09-14 20:11:15 -0400

    Added Schedule React Component
    Ability to make placeholder schedule grids added.

--- slate/src/Schedule.js
+++ slate/src/Schedule.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ScheduleBlock {
+    constructor(name, duration, start, end) {
+        this.state = {name: name, duration: duration, start: start, end: end};
+    }
+}
 
 class Schedule extends React.Component {
     constructor(){
@@ -6,31 +13,48 @@ class Schedule extends React.Component {
         this.state = {timetable: [144][7]};
     }
 
+    addTimeTable(){
+
+    }
+
     render(){
-        var schedule = [145][8];
+        var schedule = [], cols = 145;
         var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        for (var i = 0;i<145;i++){
+            schedule[i] = [];
+        }
         schedule[0][0] = (<td>
                             <h3>Time</h3>
                           </td>);
-        for (var i = 1;i<8;i++){
+        for (var i = 1;i<8;i++) {
             schedule[0][i] = (<td>
-                                <h3>{days[i-1]}</h3>
+                                <h3>{days[i - 1]}</h3>
                               </td>);
         }
-        var curtime = 0;
-        var hours = 0;
         for (var i = 1;i<145;i++){
-            schedule[i][0] = (<td>
-                                <h3>{hours.toString() + ":" + curtime.toString()}</h3>
-                              </td>);
-            curtime += 10;
-            if (curtime==60){
-                hours ++;
-                curtime = 0;
+            if ((i-1)%6==0) {
+                schedule[i][0] = (<td>
+                                    <h3>{(Math.floor(i/6)).toString() + ":00"}</h3>
+                                  </td>);
+            }
+            else {
+                schedule[i][0] = (<td>
+                                    <h3></h3>
+                                  </td>);
             }
         }
+        var format = [145];
+        for (var i = 0;i<145;i++){
+            format[i] = (<tr>
+                            {schedule[i]}
+                         </tr>);
+        }
         return(
-
+            <div>
+                <table>
+                    {format}
+                </table>
+            </div>
         );
     }
 }

c36f745b5585284d08fc3c184b2508d25eefb4b4
DannyDaoBoYang <34634047+DannyDaoBoYang@users.noreply.github.com>
2019-12-25 11:36:18 -0500

    typo

--- backend/main.py
+++ backend/main.py
@@ -132,7 +132,7 @@ def lcs(X, Y):
     n = len(Y)
 
     # declaring the array for storing the dp values
-    L = [[None] * (n + 1) for i in xrange(m + 1)]
+    L = [[None] * (n + 1) for i in range(m + 1)]
 
     """Following steps build L[m + 1][n + 1] in bottom up fashion 
     Note: L[i][j] contains length of LCS of X[0..i-1] 

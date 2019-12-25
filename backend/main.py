bb17eadf0cc76ab0f7f546e54c09f5debf026e83
DannyDaoBoYang <34634047+DannyDaoBoYang@users.noreply.github.com>
2019-12-25 11:26:36 -0500

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,6 @@ from backend.config import Config
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret'
 
@@ -18,8 +17,7 @@ firebase_admin.initialize_app(cred, {
 
 ref = db.reference('/slate')
 
-
-class User():
+class User ():
     def __init__(self, id, username, email, password, timetable, friends):
         self._id = id
         self._username = username
@@ -28,42 +26,104 @@ class User():
         self._timetable = timetable
         self._friends = friends
 
-
-class TimeSlot():
+class TimeSlot ():
     def __init__(self, start, end, day):
         self._start = start
         self._end = end
         self._day = day
 
-
-class Friend():
+class Friend ():
     def __init__(self, friendid):
         self._friendid = friendid
-
 
 @app.route('/')
 def index():
     return 'Hello'
 
-
-@app.route('/findfriend', methods=['GET'])
-def findfriend():
+@app.route('/login', methods=['POST'])
+def login():
     data = request.get_json()
-    friendname = data['friend']
-    limit = len(friendname) // 2 + 1;
-    listofchildren = ref.order_by_child("username")  # this is the list of people
-    RUTA = []
+    username = data['username']
+    password = data['password']
+    snapshot = ref.child(username).child('password').get()
+    if (password==snapshot):
+        #print(snapshot)
+        ref.child(username).update({
+            'isLoggedIn': 1
+        })
+        session['username'] = username
+        return "login successful", 200
+    return "login failed", 404
+@app.route('/logout', methods=['POST'])
+def logout():
+    username=session['username']
+    ref.child(username).update({
+        'isLoggedIn':0
+    })
+    return "logout successful", 200
+
+@app.route('/login', methods=['GET'])
+def login2():
+    username = session['username']
+    snapshot = ref.child(username).child('isLoggedIn').get()
+    if (snapshot==1):
+        return "isLoggedIn", 200
+    return "Error", 404;
+
+@app.route ('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    email = data['email']
+    ref.child(username).set({
+        'username': username,
+        'password': password,
+        'email': email,
+        'isLoggedIn': 0
+    })
+    return "successful", 200
+
+@app.route ('/password', methods=['POST'])
+def changePassword():
+    data = request.get_json()
+    username = session['username']
+    password = data['password']
+    ref.child(username).update({
+        'password': password
+    })
+    return "successful", 200
+
+@app.route ('/email', methods=['POST'])
+def changeEmail():
+    data = request.get_json()
+    username = session['username']
+    email = data['email']
+    ref.child(username).update({
+        'email': email
+    })
+    return "successful", 200
+
+
+
+@app.route('/findfriend',methods=['GET'])
+def findfriend():
+    data=request.get_json()
+    friendname=data['friendname']
+    limit = len(friendname)//2+1;
+    listofchildren=ref.order_by_child("username") #this is the list of people
+    RUTA=[]
     for friend in listofchildren:
-        # Code for query goes here
-        current = friend.child("username").get()
-        for i in range(0, min(len(friendname), len(current))):
-            k = lcs(friendname, current)
-            if (k >= limit):
+        #Code for query goes here
+        current =friend.child("username").get()
+        for i in range(0,min(len (friendname), len(current))):
+            k=lcs(friendname,current)
+            if(k >= limit):
                 RUTA.append(current)
                 print(current)
     if not RUTA:
-        return RUTA, 400  # nothing is found
-    return RUTA, 200
+        return RUTA, 400 #nothing is found
+    return RUTA,200
 
 
 def lcs(X, Y):
@@ -89,56 +149,6 @@ def lcs(X, Y):
                 # L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1]
     return L[m][n]
 
-
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    username = data['username']
-    password = data['password']
-    snapshot = ref.child(username).child('password').get()
-    if (password == snapshot):
-        # print(snapshot)
-        ref.child(username).update({
-            'isLoggedIn': 1
-        })
-        session['username'] = username
-        return "login successful", 200
-    return "login failed", 404
-
-
-@app.route('/logout', methods=['POST'])
-def logout():
-    username = session['username']
-    ref.child(username).update({
-        'isLoggedIn': 0
-    })
-    return "logout successful", 200
-
-
-@app.route('/login', methods=['GET'])
-def login2():
-    username = session['username']
-    snapshot = ref.child(username).child('isLoggedIn').get()
-    if (snapshot == 1):
-        return "isLoggedIn", 200
-    return "Error", 404;
-
-
-@app.route('/signup', methods=['POST'])
-def signup():
-    data = request.get_json()
-    username = data['username']
-    password = data['password']
-    email = data['email']
-    ref.child(username).set({
-        'username': username,
-        'password': password,
-        'email': email,
-        'isLoggedIn': 0
-    })
-    return "successful", 200
-
-
 if __name__ == '__main__':
     app.run()
 
@@ -148,13 +158,16 @@ app.config.from_object(Config)
 #Before initial, set the database URI to our database.
 db = SQLAlchemy(app)
 daysinweek = {'Monday':1, 'Tuesday':2, 'Wednesday':3, 'Thursday':4, 'Friday':5, 'Saturday':6, 'Sunday':7}
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False, unique=False)
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.password}')"
+
 class Time(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False, unique=False)
@@ -167,6 +180,7 @@ class Time(db.Model):
         return f"{{'start':'{self.start}', 'end':'{self.end}', 'day':'{self.day}'}}"
     def __lt__(self, other):
         return self.start < other.start
+
 class Friend(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False, unique=False)
@@ -175,6 +189,7 @@ class Friend(db.Model):
         return f"Friend('{self.user_id}', '{self.friend_id}')"
     def __str__(self):
         return str(self.friend_id)
+
 @app.route("/api/info")
 def api_info():
     info = {
@@ -184,10 +199,12 @@ def api_info():
        "load" : [ 3.21, 7, 14 ]
     }
     return jsonify(info)
+
 @app.route('/')
 def hello_world():
     request_finished
     return 'Hello World!',4
+
 @app.route('/login', methods = ['GET','POST'])
 def login():
     data=request.json
@@ -200,8 +217,10 @@ def login():
     except:
         db.session.commit()
         return "authentication failed", 401
+
     db.session.commit()
     return "login successful", 200
+    
 
 @app.route('/signup', methods = ['GET','POST','OPTIONS'])
 #Get returns the HTML, Post return what the code below does
@@ -211,11 +230,13 @@ def signup():
     username = data["username"]
     email = data["email"]
     password = str(data["password"])
+
     #checks if email or username already exists
     email_query = User.query.filter(User.email == email).all()
     uname_query = User.query.filter(User.username == username).all()
     if (len(email_query) > 0 or len(uname_query) > 0):
         return 'username or email already exists', 409
+
     user = User(username=username, email=email, password=hashlib.sha256(password.encode('ascii')).hexdigest())
     db.session.add(user)
     db.session.commit()
@@ -223,6 +244,8 @@ def signup():
    # print("Either Get or Option")
    # return 'signup successful', 201
     #else if the method is GET
+
+
 @app.route('/addtime', methods = ['POST'])
 def addtime():
     data = request.json
@@ -232,26 +255,27 @@ def addtime():
     end = int(data['end_h'])*60+int(data['end_m'])
     #day of the week is stored as an integer between 1 and 7
     day = daysinweek[data['day']]
-
+          
     #check validity of time interval
     if (start >= end or start < 0 or end > 1440):
         return 'invalid time interval', 409
     tquery=Time.query.filter(Time.user_id == user_id, Time.day == day).all()
-
+    
     #check if time interval overlaps with existing time intervals 
     for i in tquery:
         if (not ((i.start >= end) or (i.end <= start))):
             return 'overlapping time interval', 409
-
+    
     time = Time(user_id=user_id, start=start, end=end, day=day)
     db.session.add(time)
     db.session.commit()
     return 'time interval successfully added', 201
+
 @app.route('/gettime', methods = ['POST'])
 def gettime():
     data = request.json
     user_id = int(data['user_id'])
-
+    
     #formats user's schedule into a length 7 list of lists
     #ith list is the user's sorted list of busy time intervals on the ith day
     jsonstr = '{['
@@ -266,6 +290,7 @@ def gettime():
         jsonstr += '],'
     jsonstr = jsonstr[:-1] + ']}'
     return jsonstr, 200
+
 @app.route('/addfriend', methods = ['POST'])
 def addfriend():
     data = request.json
@@ -277,6 +302,7 @@ def addfriend():
     db.session.add(friend)
     db.session.commit()
     return 'friend added', 201
+
 @app.route('/getfriends', methods = ['POST'])
 def getfriends():
     data = request.json
@@ -289,6 +315,7 @@ def getfriends():
         jsonstr = jsonstr[:-1]
     jsonstr += ']}'
     return jsonstr, 200
+
 @app.route('/comparetime',methods = ['POST'])
 def comparetime():
     data=request.json #incomming from user
@@ -316,6 +343,7 @@ def comparetime():
                 jsonstr=jsonstr + "],"
             else:
                 jsonstr=jsonstr +"]"
+
             continue
         start=Valid[0]
         last=Valid[0]
@@ -323,6 +351,7 @@ def comparetime():
             if(Valid.contains(i)):
                 if((i==1440) and (last == i - 10)):
                     jsonstr = jsonstr + f"{{'start':'{start}', 'end':'{i}', 'day':'{day}'}}"
+
                 if (last==i-10):
                     last=i
                     continue
@@ -339,3 +368,4 @@ def comparetime():
     jsonstr=jsonstr+"}"
     return jsonstr,200
 '''
+

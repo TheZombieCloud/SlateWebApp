63662f6d518df08472ac9d6bbcf569ec4ed30848
DannyDaoBoYang <34634047+DannyDaoBoYang@users.noreply.github.com>
2019-12-31 16:12:01 -0500

    On change updates

--- slate/src/ProfilePage.js
+++ slate/src/ProfilePage.js
@@ -88,7 +88,7 @@ class ProfilePage extends React.Component{
                                  <form className = "ppform">
                                      <div className = "search">
                                          <h6 className = "searchfrt">Search for Friends</h6>
-                                         <input className ="searchBar" id="friendname" type = "text" value={this.state.friendname} onChange = {this.search} placeholder = "Search..."/>
+                                         <input className ="searchBar" id="friendname" type = "text" value={this.state.friendname} onChange = {this.handleChange} placeholder = "Search..."/>
                                      </div>
                                  </form>
                              </div>
